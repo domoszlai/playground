f489472e1f7ef2771fa3c69a3ba866bc058ec81d
domoszlai <dlacko@gmail.com>
2022-10-22 17:10:18 +0200

    Test parametric rewriting

--- l-system/app/src/test/kotlin/lsystem/RewriterTest.kt
+++ l-system/app/src/test/kotlin/lsystem/RewriterTest.kt
@@ -18,4 +18,24 @@ class RewriterTest {
         assertContentEquals(listOf(G()),  r.rewrite(listOf(F()), 1))
         assertContentEquals(listOf(H()),  r.rewrite(listOf(F()), 2))
     }
+
+    @Test fun basicParametric() {
+
+        data class A(val x: Int, val y: Int) : TurtleRewriterNode()
+        data class B(val x: Int) : TurtleRewriterNode()
+        data class C(val dummy: Unit = Unit) : TurtleRewriterNode()
+
+        val r = ParametricRewriter<TurtleRewriterNode>()
+        r.addRule(A::class, {it.y <= 3}, {listOf(A(it.x*2, it.x+it.y))})
+        r.addRule(A::class, {it.y > 3}, {listOf(B(it.x), A(it.x/it.y, 0))})
+        r.addRule(B::class, {it.x < 1}, {listOf(C())})
+        r.addRule(B::class, {it.x >= 1}, {listOf(B(it.x-1))})
+
+        val axiom = listOf(B(2), A(4,4))
+
+        assertContentEquals(listOf(B(1), B(4), A(1,0)), r.rewrite(axiom, 1))
+        assertContentEquals(listOf(B(0), B(3), A(2,1)), r.rewrite(axiom, 2))
+        assertContentEquals(listOf(C(), B(2), A(4,3)), r.rewrite(axiom, 3))
+        assertContentEquals(listOf(C(), B(1), A(8,7)), r.rewrite(axiom, 4))
+    }
 }
